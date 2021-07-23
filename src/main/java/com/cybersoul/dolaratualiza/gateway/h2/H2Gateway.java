edaf37d5eca51d534a47cceea8c5b6efd87d32d3
Pablo <pablogomescorrea@ibm.com>
2021-07-23 20:30:32 -0300

    Removing logs

--- src/main/java/com/cybersoul/dolaratualiza/gateway/h2/H2Gateway.java
+++ src/main/java/com/cybersoul/dolaratualiza/gateway/h2/H2Gateway.java
@@ -1,8 +1,6 @@
 package com.cybersoul.dolaratualiza.gateway.h2;
 
 import com.cybersoul.dolaratualiza.entity.DolarEntity;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -14,15 +12,11 @@ public class H2Gateway {
     @Autowired
     private H2Repository h2Repository;
 
-    private static final Logger LOG = LoggerFactory.getLogger(H2Gateway.class);
-
     public void execute(DolarEntity entity) {
 
         if (Objects.nonNull(entity)) {
             h2Repository.save(entity);
-            LOG.info("" + entity);
         } else {
-            LOG.error("" + entity);
             throw new NullPointerException();
         }
 	}
