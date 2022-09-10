957beeb3f65f7a8ae2f7a839d8766756641e484d
Pablo Correa <pablocorrea@MacBook-Pro-de-Pablo.local>
2022-09-10 02:16:25 -0300

    Gravacao de diversas moedas do dia para estatistica

--- src/main/java/com/cybersoul/dolaratualiza/gateway/rest/CurrencyOfDayController.java
+++ src/main/java/com/cybersoul/dolaratualiza/gateway/rest/CurrencyOfDayController.java
@@ -1,7 +1,5 @@
 package com.cybersoul.dolaratualiza.gateway.rest;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
